edcda8370671570d6fc19c9edd4786b504e133e9
Christopher Krier <ckrier.3000@gmail.com>
2016-10-16 20:35:01 -0400

    Add First Box Tracer Implementation

--- src/main/java/tk/coaster3000/pocraytrace/item/DebugItem.java
+++ src/main/java/tk/coaster3000/pocraytrace/item/DebugItem.java
@@ -7,8 +7,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import tk.coaster3000.pocraytrace.tracer.BoxTracer;
+import tk.coaster3000.pocraytrace.tracer.TracerResult;
+import tk.coaster3000.pocraytrace.tracer.Tracers;
 
 public class DebugItem extends Item {
 	public DebugItem(String name) {
@@ -18,9 +23,47 @@ public class DebugItem extends Item {
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
-		if (!worldIn.isRemote) playerIn.addChatMessage(new TextComponentString("Hello Player"));
+	public boolean onDroppedByPlayer(ItemStack item, EntityPlayer player) {
+		item.clearCustomName();
+		player.addChatComponentMessage(new TextComponentString("Cleared Tracer Mode"));
+		return false;
+	}
 
-		return ActionResult.newResult(EnumActionResult.SUCCESS, itemStackIn);
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		Tracers tracer = Tracers.BOX;
+		if (itemStackIn.hasDisplayName()) tracer = Tracers.valueOf(itemStackIn.getDisplayName());
+
+		if (playerIn.isSneaking()) {
+			int o = tracer.ordinal() + 1; //Get Ordinal and increment
+			if (o >= Tracers.values().length) o = 0;
+			tracer = Tracers.values()[o];
+			if (worldIn.isRemote) playerIn.addChatMessage(new TextComponentString("Changed to tracer mode: " + tracer.toString()));
+			return ActionResult.newResult(EnumActionResult.SUCCESS, itemStackIn.setStackDisplayName(tracer.name()));
+		} else {
+			TracerResult trace = tracer.trace(worldIn, playerIn.getPosition());
+			StringBuilder sb = new StringBuilder();
+
+			if (trace instanceof BoxTracer.BoxTracerResult) {
+				BoxTracer.BoxTracerResult btr = (BoxTracer.BoxTracerResult)trace;
+
+				sb.append("Box Tracer Log").append('\n');
+				sb.append("Player Block Pos: (X) ").append(playerIn.getPosition().getX()).append(" (Y) ").append(playerIn.getPosition().getY()).append(" (Z) ").append(playerIn.getPosition().getZ()).append("\n\n");
+				BlockPos point = btr.getStartPoint();
+				sb.append("Start X: ").append(point.getX()).append('\n').append("Start Y: ").append(point.getY()).append('\n').append("Start Z: ").append(point.getZ());
+				sb.append('\n').append('\n');
+				point = btr.getEndPoint();
+				sb.append("End X: ").append(point.getX()).append('\n').append("End Y: ").append(point.getY()).append('\n').append("End Z: ").append(point.getZ());
+			} else {
+				sb.append("Tracer Not Implemented");
+			}
+
+			FMLCommonHandler.instance().getFMLLogger().info(sb.toString());
+
+
+		}
+
+		return ActionResult.newResult(EnumActionResult.SUCCESS, itemStackIn.setStackDisplayName(tracer.name()));
+
 	}
 }
--- src/main/java/tk/coaster3000/pocraytrace/tracer/BoxTracer.java
+++ src/main/java/tk/coaster3000/pocraytrace/tracer/BoxTracer.java
@@ -1,19 +1,67 @@
 package tk.coaster3000.pocraytrace.tracer;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
-import java.util.List;
-
-class BoxTracer extends Tracer {
+public class BoxTracer extends Tracer {
 
 	@Override
 	public TracerResult trace(World world, BlockPos start) {
-//		if (!world.getBlockState(start).getBlock().isAir(world.getBlockState(start), world, start)) {
+		if (!world.getBlockState(start).getBlock().isAir(world.getBlockState(start), world, start)) {
 			return TracerResult.getNullTracer();
-//		}
+		}
 
+		int north = 0, south = 0, east = 0, west = 0;
+		BlockPos cursor = start.north();
+		IBlockState state;
+		while ((state = world.getBlockState(cursor = cursor.north())).getBlock().isAir(state, world, cursor)) {
+			north++;
+		}
 
+		cursor = start.south();
+		while ((state = world.getBlockState(cursor = cursor.south())).getBlock().isAir(state, world, cursor)) {
+			south++;
+		}
+
+		cursor = start.east();
+		while ((state = world.getBlockState(cursor = cursor.east())).getBlock().isAir(state, world, cursor)) {
+			east++;
+		}
+
+		cursor = start.west();
+		while ((state = world.getBlockState(cursor = cursor.west())).getBlock().isAir(state, world, cursor)) {
+			west++;
+		}
+		BlockPos p1 = start.north(north).east(east), p2 = start.south(south).west(west);
+
+		return new BoxTracerResult(p1, p2);
+	}
+
+	public static class BoxTracerResult extends TracerResult {
+
+		final AxisAlignedBB boundingBox;
+		BoxTracerResult(BlockPos p1, BlockPos p2) {
+			boundingBox = new AxisAlignedBB(p1, p2);
+		}
+
+		public BlockPos getStartPoint() {
+			return new BlockPos(boundingBox.minX, boundingBox.minY, boundingBox.minZ);
+		}
+
+		public BlockPos getEndPoint() {
+			return new BlockPos(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ);
+		}
+
+		public AxisAlignedBB getBoundingBox() {
+			return boundingBox;
+		}
+
+		@Override
+		public boolean contains(BlockPos position) {
+			return boundingBox.isVecInside(new Vec3d(position.getX(), position.getY(), position.getZ()));
+		}
 	}
 }
